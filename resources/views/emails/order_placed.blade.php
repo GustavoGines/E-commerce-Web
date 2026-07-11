81b46041af1ce9b2ef8bee2641178bf826fe598e
GustavoGines <ginesparker95@gmail.com>
2026-07-11 02:52:30 -0300

    Fix: simplificar texto del email quitando metodos de pago

--- resources/views/emails/order_placed.blade.php
+++ resources/views/emails/order_placed.blade.php
@@ -14,7 +14,7 @@ Hemos recibido tu pedido **#{{ str_pad($order->id, 5, '0', STR_PAD_LEFT) }}** co
 | **Total:** | | **${{ number_format($order->total, 2) }}** |
 </x-mail::table>
 
-Como tu método de pago es **"Acordado con el Vendedor"** (transferencia o efectivo), nos pondremos en contacto contigo a la brevedad para coordinar el pago y la entrega de tus productos.
+Nos pondremos en contacto contigo a la brevedad para coordinar el pago y la entrega de tu pedido.
 
 <x-mail::button :url="route('my-orders')" color="primary">
 Ver Mis Órdenes
