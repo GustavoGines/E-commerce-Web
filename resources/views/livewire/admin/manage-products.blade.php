798c9bc46b3a3ea388ec2d46525f6ef9928c20a3
GustavoGines <ginesparker95@gmail.com>
2026-07-18 20:09:16 -0300

    fix: restaurar preview de imagen al hacer click en el modal de edicion

--- resources/views/livewire/admin/manage-products.blade.php
+++ resources/views/livewire/admin/manage-products.blade.php
@@ -1181,8 +1181,11 @@ new #[Layout('layouts.app')] class extends Component {
                             <div class="flex flex-col sm:flex-row gap-3">
                                 @if($current_image_url)
                                     <div class="w-full sm:w-auto p-2 bg-gray-50 dark:bg-gray-800/50 rounded-xl border border-gray-200 dark:border-gray-700 flex items-center space-x-3 transition-all duration-300 shrink-0">
-                                        <div class="relative group cursor-pointer shrink-0" @click="expandedImage = !expandedImage">
-                                            <img src="{{ asset('storage/' . $current_image_url) }}" alt="Imagen actual" class="h-14 w-14 sm:h-16 sm:w-16 object-cover rounded-lg border border-gray-200 dark:border-gray-600 shadow-sm bg-white dark:bg-gray-900">
+                                        <div class="relative group cursor-pointer shrink-0" @click="previewImageUrl = '{{ asset('storage/' . $current_image_url) }}'; previewImageOpen = true">
+                                            <img src="{{ asset('storage/' . $current_image_url) }}" alt="Imagen actual" class="h-14 w-14 sm:h-16 sm:w-16 object-cover rounded-lg border border-gray-200 dark:border-gray-600 shadow-sm bg-white dark:bg-gray-900 hover:opacity-80 transition-opacity" title="Clic para agrandar">
+                                            <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
+                                                <svg class="w-5 h-5 text-white drop-shadow-lg" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM10 7v3m0 0v3m0-3h3m-3 0H7"/></svg>
+                                            </div>
                                         </div>
                                         <div class="flex flex-col flex-1">
                                             <h5 class="text-[10px] sm:text-xs font-bold text-gray-900 dark:text-white mb-0.5">Imagen Actual</h5>
