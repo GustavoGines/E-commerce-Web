bd617008c1f4218e242c29b22f81e7d041c9839e
GustavoGines <ginesparker95@gmail.com>
2026-07-18 20:33:59 -0300

    ux: ocultar miniatura al expandir imagen en el formulario

--- resources/views/livewire/admin/manage-products.blade.php
+++ resources/views/livewire/admin/manage-products.blade.php
@@ -1188,8 +1188,8 @@ new #[Layout('layouts.app')] class extends Component {
                                         </div>
 
                                         <!-- Fila normal con miniatura + borrar -->
-                                        <div class="flex items-center space-x-3">
-                                            <div class="relative group cursor-pointer shrink-0" @click="expandedImage = !expandedImage" title="Clic para agrandar">
+                                        <div x-show="!expandedImage" x-transition class="flex items-center space-x-3">
+                                            <div class="relative group cursor-pointer shrink-0" @click="expandedImage = true" title="Clic para agrandar">
                                                 <img src="{{ asset('storage/' . $current_image_url) }}" alt="Imagen actual" class="h-14 w-14 sm:h-16 sm:w-16 object-cover rounded-lg border border-gray-200 dark:border-gray-600 shadow-sm bg-white dark:bg-gray-900 hover:opacity-80 transition-opacity">
                                                 <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 group-active:opacity-100 transition-opacity bg-black/10 rounded-lg">
                                                     <svg class="w-5 h-5 text-white drop-shadow-lg" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM10 7v3m0 0v3m0-3h3m-3 0H7"/></svg>
